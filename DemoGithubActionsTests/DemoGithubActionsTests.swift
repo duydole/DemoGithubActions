f35c7a92ac786e1669a9efdeccc416c7b2e82aea
Đỗ Lê Duy <duy.do@livepayments.com>
2025-05-06 20:36:52 +0700

    Update

---
+++ DemoGithubActionsTests/DemoGithubActionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGithubActionsTests.swift
+//  DemoGithubActionsTests
+//
+//  Created by Đỗ Lê Duy on 6/5/25.
+//
+
+import Testing
+@testable import DemoGithubActions
+
+struct DemoGithubActionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
