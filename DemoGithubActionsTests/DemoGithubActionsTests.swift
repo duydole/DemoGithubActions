00f813dbfdb86562f9f25d4963d82a8e873232cf
Đỗ Lê Duy <duy.do@livepayments.com>
2025-06-16 13:46:04 +0700

    Update

--- DemoGithubActionsTests/DemoGithubActionsTests.swift
+++ DemoGithubActionsTests/DemoGithubActionsTests.swift
@@ -14,4 +14,23 @@ struct DemoGithubActionsTests {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
+//    @Test func testAdd() async throws {
+//        let result = Test2.add(2, 3)
+//        #expect(result == 5)
+//    }
+//    
+//    @Test func testSubtract() async throws {
+//        let result = Test2.subtract(5, 3)
+//        #expect(result == 2)
+//    }
+//    
+//    @Test func testMultiply() async throws {
+//        let result = Test2.multiply(2, 3)
+//        #expect(result == 6)
+//    }
+//    
+//    @Test func testDivide() async throws {
+//        let result = Test2.divide(6, 3)
+//        #expect(result == 2)
+//    }
 }
