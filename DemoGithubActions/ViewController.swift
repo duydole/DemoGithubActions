0becedbb7e30d4a74d8536d736c3f1e015b6361e
Đỗ Lê Duy <duy.do@livepayments.com>
2025-06-17 10:52:23 +0700

    Update

---
+++ DemoGithubActions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoGithubActions
+//
+//  Created by Đỗ Lê Duy on 6/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
---
+++ DemoGithubActionsTests/DemoGithubActionsTests.swift
@@ -0,0 +1,41 @@
+//
+//  DemoGithubActionsTests.swift
+//  DemoGithubActionsTests
+//
+//  Created by Đỗ Lê Duy on 6/5/25.
+//
+
+import Testing
+@testable import DemoGithubActions
+
+struct DemoGithubActionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+    @Test func testAdd() async throws {
+        let result = Math.add(2, 3)
+        #expect(result == 5)
+    }
+    
+    @Test func testSubtract() async throws {
+        let result = Math.subtract(5, 3)
+        #expect(result == 2)
+    }
+    
+    @Test func testMultiply() async throws {
+        let result = Math.multiply(2, 3)
+        #expect(result == 6)
+    }
+    
+    @Test func testDivide() async throws {
+        let result = Math.divide(6, 3)
+        #expect(result == 2)
+    }
+    
+    @Test func testHelloWorld() async throws {
+        let result = Math.hello()
+        #expect(result == "hello")
+    }
+}
